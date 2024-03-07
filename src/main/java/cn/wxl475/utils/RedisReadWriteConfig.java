a3ab3072cfe7d3deb47c8206616f6f0792719edb
wxl <1715142548@qq.com>
2024-03-07 22:02:25 +0800

    redis读写分离

---
+++ src/main/java/cn/wxl475/utils/RedisReadWriteConfig.java
@@ -0,0 +1,15 @@
+package cn.wxl475.utils;
+
+import io.lettuce.core.ReadFrom;
+import org.springframework.boot.autoconfigure.data.redis.LettuceClientConfigurationBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RedisReadWriteConfig
+{
+    @Bean
+    public LettuceClientConfigurationBuilderCustomizer clientConfigurationBuilderCustomizer(){
+        return clientConfigurationBuilder -> clientConfigurationBuilder.readFrom(ReadFrom.REPLICA_PREFERRED);
+    }
+}
