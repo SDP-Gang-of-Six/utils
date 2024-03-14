5db474b82466fdda05441f41abd50d2f5e1506eb
Cbgogogog <1293543528@qq.com>
2024-03-15 01:55:22 +0800

    feat: 完善question

--- src/main/java/cn/wxl475/redis/RedisConstants.java
+++ src/main/java/cn/wxl475/redis/RedisConstants.java
@@ -7,7 +7,7 @@ public class RedisConstants {
 //    public static final String LOCK_GOODS_KEY="lock:goods:";
     public static final Long LOCK_GOODS_TTL=10L;
     public static final String CACHE_QUESTION_KEY="cache:question:";
-    public static final Long CACHE_QUESTION_TTL=10L;
+    public static final Long CACHE_QUESTION_TTL=30L;
     public static final String CACHE_PAPER_KEY="cache:paper:";
     public static final String CACHE_EXAM_KEY="cache:exam:";
     public static final String CACHE_USERS_KEY="cache:users:";
