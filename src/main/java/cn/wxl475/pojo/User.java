ae349cc9f22d048ff6b0f8505348158dca7d7eac
pc <2528578653@qq.com>
2024-03-14 16:54:11 +0800

    用户分页查询，登陆注册接口完成

--- src/main/java/cn/wxl475/pojo/User.java
+++ src/main/java/cn/wxl475/pojo/User.java
@@ -11,28 +11,22 @@ import java.time.LocalDateTime;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@TableName("user")
+@TableName(value = "user", schema = "user")
 public class User {
     //对应数据库中的主键（UUID、自增id、雪花算法、redis、zookeeper）
     @TableId(type = IdType.ASSIGN_ID)
     private Long uid;
 
-    @TableField("nickname")
     private String nickname;
 
-    @TableField("password")
     private String password;
 
-    @TableField("username")
     private String username;
 
-    @TableField("user_type")
     private Boolean userType;
 
-    @TableField("deleted")
     private Boolean deleted;
 
-    @TableField("head_url")
     private String headUrl;
 
     //注解填充字段 @TableField(.. fill = FieldFill.INSERT) 生成器策略部分也可以配置！
