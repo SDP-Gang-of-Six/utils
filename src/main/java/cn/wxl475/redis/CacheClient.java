dff8d0a7c9ee922c69945318a6f6a9bb1fd02443
Cbgogogog <1293543528@qq.com>
2024-03-16 18:52:29 +0800

    feat: 使用加盐解决缓存雪崩，使用互斥锁解决缓存击穿，使用回传空值解决缓存穿透

--- src/main/java/cn/wxl475/redis/CacheClient.java
+++ src/main/java/cn/wxl475/redis/CacheClient.java
@@ -1,9 +1,12 @@
 package cn.wxl475.redis;
 
+import cn.hutool.core.thread.ThreadUtil;
 import cn.hutool.core.util.BooleanUtil;
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.json.JSONObject;
 import cn.hutool.json.JSONUtil;
+import cn.wxl475.pojo.Question;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import cn.wxl475.pojo.RedisData;
 import org.springframework.stereotype.Component;
@@ -11,11 +14,13 @@ import org.springframework.stereotype.Component;
 import java.time.LocalDateTime;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
 
 import static cn.wxl475.redis.RedisConstants.*;
 
+@Slf4j
 @Component
 public class CacheClient {
     public final StringRedisTemplate stringRedisTemplate;
@@ -29,6 +34,11 @@ public class CacheClient {
         stringRedisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(value),time,unit);
     }
 
+    public void setWithRandomExpire(String key, Object value, Long time, TimeUnit unit){
+        long randomTime = time + (long)(Math.random() * 10);
+        stringRedisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(value), randomTime, unit);
+    }
+
     public void delete(String key) {
         stringRedisTemplate.delete(key);
     }
@@ -40,7 +50,7 @@ public class CacheClient {
         stringRedisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(redisData));
     }
     //回写“”值解决缓存穿透
-    public <R,ID> R queryWithPassThrough(String keyPrefix, ID id , Class<R> type, Function<ID,R> dbFallback,Long time, TimeUnit unit){
+    public <R,ID> R queryWithPassThrough(String keyPrefix, String lockKeyPrefix, ID id , Class<R> type, Function<ID,R> dbFallback,Long time, TimeUnit unit){
         String key=keyPrefix+id;
         //查询redis
         String json=stringRedisTemplate.opsForValue().get(key);
@@ -52,17 +62,22 @@ public class CacheClient {
             return null;
         }
         //查询数据库
-        R r=dbFallback.apply(id);
-        if(r ==null){
-            stringRedisTemplate.opsForValue().set(key,"",CACHE_NULL_TTL,TimeUnit.MINUTES);
-            return null;
+        String lockKey=lockKeyPrefix+id;
+        boolean isLock=tryLock(lockKey);
+        if(isLock){
+            R r = dbFallback.apply(id);
+            if(r ==null){
+                stringRedisTemplate.opsForValue().set(key,"",CACHE_NULL_TTL,TimeUnit.MINUTES);
+            }else {
+                this.setWithRandomExpire(key,r,time,unit);
+            }
+            unLock(lockKey);
         }
-        this.set(key,r,time,unit);
-        return r;
+        return null;
     }
 
     //逻辑有效期解决缓存击穿
-    public <R,ID> R queryWithLogicalExpire(String keyPrefix, ID id,Class<R> type,Function<ID,R> dbFallback,Long time, TimeUnit unit){
+    public <R,ID> R queryWithLogicalExpire(String keyPrefix, String lockKeyPrefix, ID id, Class<R> type, Function<ID,R> dbFallback,Long time, TimeUnit unit){
         String key=keyPrefix+id;
         String json = stringRedisTemplate.opsForValue().get(key);
         if(StrUtil.isBlank(json)){
@@ -77,7 +92,7 @@ public class CacheClient {
             return r;
         }
         //已过期，需要重建缓存
-        String lockKey=keyPrefix+id;
+        String lockKey=lockKeyPrefix+id;
         boolean isLock=tryLock(lockKey);
         if(isLock){
             CACHE_REBUILD_EXECUTOR.submit(()->{
@@ -95,6 +110,27 @@ public class CacheClient {
         }
         return r;
     }
+
+    public <R,ID> void resetKey(String keyPrefix, String lockKeyPrefix, ID id, Class<R> type, Function<ID,R> dbFallback,Long time, TimeUnit unit){
+        String lockKey = lockKeyPrefix+id;
+        String Key = keyPrefix+id;
+        boolean isLock=tryLock(lockKey);
+        if(isLock){
+            CACHE_REBUILD_EXECUTOR.submit(()->{
+                try{
+                    R r1= dbFallback.apply(id);
+                    this.setWithRandomExpire(Key,r1,time,unit);
+                }
+                catch (Exception e){
+                    throw new RuntimeException(e);
+                }
+                finally {
+                    unLock(lockKey);
+                }
+            });
+        }
+    }
+
 
     private boolean tryLock(String key){
         Boolean flag = stringRedisTemplate.opsForValue().setIfAbsent(key,"1",LOCK_GOODS_TTL,TimeUnit.SECONDS);
--- src/main/java/cn/wxl475/redis/RedisConstants.java
+++ src/main/java/cn/wxl475/redis/RedisConstants.java
@@ -3,8 +3,6 @@ package cn.wxl475.redis;
 public class RedisConstants {
 //    public static final Long CACHE_GOODS_TTL=30L;
     public static final Long CACHE_NULL_TTL=2L;
-//    public static final String CACHE_GOODS_KEY="cache:goods:";
-//    public static final String LOCK_GOODS_KEY="lock:goods:";
     public static final Long LOCK_GOODS_TTL=10L;
     public static final String CACHE_QUESTION_KEY="cache:question:";
     public static final Long CACHE_QUESTION_TTL=30L;
@@ -12,6 +10,8 @@ public class RedisConstants {
     public static final String CACHE_EXAM_KEY="cache:exam:";
     public static final String CACHE_USERS_KEY="cache:users:";
     public static final String LOCK_USERS_KEY="lock:users:";
+    public static final Long LOCK_USERS_TTL=10L;
+    public static final String LOCK_QUESTION_KEY = "lock:question";
     public static final Long CACHE_USERS_TTL=30L;
     public static final String CACHE_ILLNESS_KEY="cache:illness:";
     public static final String LOCK_ILLNESS_KEY="lock:illness:";
