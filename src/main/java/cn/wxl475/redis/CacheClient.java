ce3aaaac10366685251d21c9051ff6bbfa6b4a7b
wxl <1715142548@qq.com>
2024-03-16 19:54:04 +0800

    完善redis方法

--- src/main/java/cn/wxl475/redis/CacheClient.java
+++ src/main/java/cn/wxl475/redis/CacheClient.java
@@ -1,24 +1,22 @@
 package cn.wxl475.redis;
 
-import cn.hutool.core.thread.ThreadUtil;
 import cn.hutool.core.util.BooleanUtil;
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.json.JSONObject;
 import cn.hutool.json.JSONUtil;
-import cn.wxl475.pojo.Question;
+import cn.wxl475.pojo.RedisData;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.StringRedisTemplate;
-import cn.wxl475.pojo.RedisData;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
 
-import static cn.wxl475.redis.RedisConstants.*;
+import static cn.wxl475.redis.RedisConstants.CACHE_NULL_TTL;
+import static cn.wxl475.redis.RedisConstants.LOCK_GOODS_TTL;
 
 @Slf4j
 @Component
@@ -26,57 +24,108 @@ public class CacheClient {
     public final StringRedisTemplate stringRedisTemplate;
     private static final ExecutorService CACHE_REBUILD_EXECUTOR = Executors.newFixedThreadPool(10);
 
+    /**
+     * 构造方法
+     * @param stringRedisTemplate
+     */
     public CacheClient(StringRedisTemplate stringRedisTemplate){
         this.stringRedisTemplate=stringRedisTemplate;
     }
 
-    public void set(String key, Object value, Long time, TimeUnit unit){
-        stringRedisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(value),time,unit);
-    }
-
-    public void setWithRandomExpire(String key, Object value, Long time, TimeUnit unit){
-        long randomTime = time + (long)(Math.random() * 10);
-        stringRedisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(value), randomTime, unit);
-    }
-
-    public void delete(String key) {
-        stringRedisTemplate.delete(key);
-    }
-
-    public void setWithLogicalExpire(String key, Object value, Long time, TimeUnit unit){
-        RedisData redisData = new RedisData();
-        redisData.setData(value);
-        redisData.setExpireTime(LocalDateTime.now().plusSeconds(unit.toSeconds(time)));
-        stringRedisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(redisData));
-    }
-    //回写“”值解决缓存穿透
+    /**
+     * 查询并设置缓存方法，回写“”值解决缓存穿透，有效期加盐解决缓存雪崩，互斥锁访问数据库解决缓存击穿
+     * @param keyPrefix
+     * @param lockKeyPrefix
+     * @param id
+     * @param type
+     * @param dbFallback
+     * @param time
+     * @param unit
+     * @return R
+     */
     public <R,ID> R queryWithPassThrough(String keyPrefix, String lockKeyPrefix, ID id , Class<R> type, Function<ID,R> dbFallback,Long time, TimeUnit unit){
         String key=keyPrefix+id;
         //查询redis
         String json=stringRedisTemplate.opsForValue().get(key);
         if(StrUtil.isNotBlank(json)){
+            //不是“”值
             return JSONUtil.toBean(json,type);
         }
-        //redis中缓存的临时“”值解决缓存穿透问题
         if(json!=null){
+            //是redis中缓存的临时“”值
             return null;
         }
-        //查询数据库
+        //没查到缓存，也不是临时“”值，需要查询数据库
+        //互斥锁上锁
         String lockKey=lockKeyPrefix+id;
         boolean isLock=tryLock(lockKey);
+        R r = null;
         if(isLock){
-            R r = dbFallback.apply(id);
+            //查询数据库
+            r = dbFallback.apply(id);
+            //数据库中没有对应数据，回写空值
             if(r ==null){
                 stringRedisTemplate.opsForValue().set(key,"",CACHE_NULL_TTL,TimeUnit.MINUTES);
-            }else {
+            }
+            //数据库中有数据，随机有效期设置缓存
+            else {
                 this.setWithRandomExpire(key,r,time,unit);
             }
             unLock(lockKey);
         }
-        return null;
+        return r;
     }
 
-    //逻辑有效期解决缓存击穿
+    /**
+     * 更新缓存
+     * @param keyPrefix
+     * @param lockKeyPrefix
+     * @param id
+     * @param type
+     * @param dbFallback
+     * @param time
+     * @param unit
+     */
+    public <R,ID> void resetKey(String keyPrefix, String lockKeyPrefix, ID id, Class<R> type, Function<ID,R> dbFallback,Long time, TimeUnit unit){
+        String lockKey = lockKeyPrefix+id;
+        String Key = keyPrefix+id;
+        boolean isLock=tryLock(lockKey);
+        if(isLock){
+            CACHE_REBUILD_EXECUTOR.submit(()->{
+                try{
+                    R r1= dbFallback.apply(id);
+                    this.setWithRandomExpire(Key,r1,time,unit);
+                }
+                catch (Exception e){
+                    throw new RuntimeException(e);
+                }
+                finally {
+                    unLock(lockKey);
+                }
+            });
+        }
+    }
+
+    /**
+     * 删除缓存方法
+     * @param key
+     */
+    public void delete(String key) {
+        stringRedisTemplate.delete(key);
+    }
+
+    /**
+     * 设置逻辑过期时间解决缓存击穿（弃用
+     * @param keyPrefix
+     * @param lockKeyPrefix
+     * @param id
+     * @param type
+     * @param dbFallback
+     * @param time
+     * @param unit
+     * @return R
+     */
+    @Deprecated
     public <R,ID> R queryWithLogicalExpire(String keyPrefix, String lockKeyPrefix, ID id, Class<R> type, Function<ID,R> dbFallback,Long time, TimeUnit unit){
         String key=keyPrefix+id;
         String json = stringRedisTemplate.opsForValue().get(key);
@@ -111,32 +160,58 @@ public class CacheClient {
         return r;
     }
 
-    public <R,ID> void resetKey(String keyPrefix, String lockKeyPrefix, ID id, Class<R> type, Function<ID,R> dbFallback,Long time, TimeUnit unit){
-        String lockKey = lockKeyPrefix+id;
-        String Key = keyPrefix+id;
-        boolean isLock=tryLock(lockKey);
-        if(isLock){
-            CACHE_REBUILD_EXECUTOR.submit(()->{
-                try{
-                    R r1= dbFallback.apply(id);
-                    this.setWithRandomExpire(Key,r1,time,unit);
-                }
-                catch (Exception e){
-                    throw new RuntimeException(e);
-                }
-                finally {
-                    unLock(lockKey);
-                }
-            });
-        }
+    /**
+     * 普通缓存设置方法
+     * @param key
+     * @param value
+     * @param time
+     * @param unit
+     */
+    private void set(String key, Object value, Long time, TimeUnit unit){
+        stringRedisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(value),time,unit);
     }
 
+    /**
+     * 有效期加盐缓存设置方法
+     * @param key
+     * @param value
+     * @param time
+     * @param unit
+     */
+    private void setWithRandomExpire(String key, Object value, Long time, TimeUnit unit){
+        long randomTime = time + (long)(Math.random() * 10);
+        this.set(key, value, randomTime, unit);
+    }
 
+    /**
+     * 设置逻辑有效期缓存方法
+     * @param key
+     * @param value
+     * @param time
+     * @param unit
+     * @return
+     */
+    private void setWithLogicalExpire(String key, Object value, Long time, TimeUnit unit){
+        RedisData redisData = new RedisData();
+        redisData.setData(value);
+        redisData.setExpireTime(LocalDateTime.now().plusSeconds(unit.toSeconds(time)));
+        stringRedisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(redisData));
+    }
+
+    /**
+     * 互斥锁上锁方法
+     * @param key
+     * @return
+     */
     private boolean tryLock(String key){
         Boolean flag = stringRedisTemplate.opsForValue().setIfAbsent(key,"1",LOCK_GOODS_TTL,TimeUnit.SECONDS);
         return BooleanUtil.isTrue(flag);
     }
 
+    /**
+     * 互斥锁解锁方法
+     * @param key
+     */
     private void unLock(String key){
         stringRedisTemplate.delete(key);
     }
