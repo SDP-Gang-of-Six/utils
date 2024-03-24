7a6bd87da4412681f321f19c634d79230b4980c9
wxl <1715142548@qq.com>
2024-03-24 15:38:36 +0800

    图片上传至es

--- src/main/java/cn/wxl475/pojo/Image.java
+++ src/main/java/cn/wxl475/pojo/Image.java
@@ -1,9 +1,15 @@
 package cn.wxl475.pojo;
 
 import com.baomidou.mybatisplus.annotation.*;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.DateFormat;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
 
 import java.time.LocalDateTime;
 
@@ -11,21 +17,32 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @TableName(value = "images",schema = "data")
+@Document(indexName = "images")
 public class Image {
     //对应数据库中的主键（UUID、自增id、雪花算法、redis、zookeeper）
     @TableId(type = IdType.INPUT)
+    @Id
+    @Field(type = FieldType.Long, store = true,index = false)
     private Long imageId;
+    @Field(type = FieldType.Long, store = true,index = false)
     private Long userId;
+    @Field(type = FieldType.Text, store = true,index = false)
     private String imageUrl;
+    @Field(type = FieldType.Text, store = true, analyzer = "ik_max_word",searchAnalyzer = "ik_smart")
     private String imageName;
+    @Field(type = FieldType.Text, store = true,index = false)
     private String imageType;
+    @Field(type = FieldType.Long, store = true,index = false)
     private Long imageSize;
     //注解填充字段 @TableField(.. fill = FieldFill.INSERT) 生成器策略部分也可以配置！
     @TableField(fill = FieldFill.INSERT)
-    private LocalDateTime createTime;
+    @Field(type = FieldType.Text, store = true,index = false)
+    private String createTime;
     //注解填充字段 @TableField(.. fill = FieldFill.INSERT) 生成器策略部分也可以配置！
     @TableField(fill = FieldFill.INSERT_UPDATE)
-    private LocalDateTime updateTime;
+    @Field(type = FieldType.Text, store = true,index = false)
+    private String updateTime;
     @TableLogic
+    @Field(type = FieldType.Boolean, store = true,index = false)
     private Boolean Deleted;
 }
