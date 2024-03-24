57a87a1dd21d1da7a7794e88e04de960a91bdbac
pc <2528578653@qq.com>
2024-03-24 23:17:57 +0800

    病例实体类es注解添加

--- src/main/java/cn/wxl475/pojo/Illness.java
+++ src/main/java/cn/wxl475/pojo/Illness.java
@@ -6,6 +6,10 @@ import com.baomidou.mybatisplus.extension.handlers.JacksonTypeHandler;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -14,39 +18,53 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @TableName(value = "illness", schema = "illness", autoResultMap = true)
+@Document(indexName = "images")
 public class Illness {
     @TableId(type = IdType.ASSIGN_ID)
+    @Id
+    @Field(type = FieldType.Long, store = true,index = false)
     private Long illnessId;
 
+    @Field(type = FieldType.Keyword, store = true, analyzer = "ik_max_word",searchAnalyzer = "ik_smart")
     private String illnessName;
 
+    @Field(type = FieldType.Integer, store = true,index = false)
     private Integer illnessType;
 
     // 症状
+    @Field(type = FieldType.Text, store = true, analyzer = "ik_max_word",searchAnalyzer = "ik_smart")
     private String symptom;
 
     // 检查过程
+    @Field(type = FieldType.Text, store = true, analyzer = "ik_max_word",searchAnalyzer = "ik_smart")
     private String process;
 
     // 诊断结果
+    @Field(type = FieldType.Text, store = true, analyzer = "ik_max_word",searchAnalyzer = "ik_smart")
     private String consequence;
 
     // 治疗方案
+    @Field(type = FieldType.Text, store = true, analyzer = "ik_max_word",searchAnalyzer = "ik_smart")
     private String schedule;
 
     @TableField(typeHandler = JacksonTypeHandler.class)
+    @Field(type = FieldType.Long, store = true,index = false)
     private List<Long> imageIds;
 
     @TableField(typeHandler = JacksonTypeHandler.class)
+    @Field(type = FieldType.Long, store = true,index = false)
     private List<Long> videoIds;
 
     @TableLogic
+    @Field(type = FieldType.Boolean, store = true,index = false)
     private Boolean deleted;
 
     //注解填充字段 @TableField(.. fill = FieldFill.INSERT) 生成器策略部分也可以配置！
     @TableField(fill = FieldFill.INSERT)
+    @Field(type = FieldType.Text, store = true, index = false)
     private String createTime;
 
     @TableField(fill = FieldFill.INSERT_UPDATE)
+    @Field(type = FieldType.Text, store = true, index = false)
     private String updateTime;
 }
