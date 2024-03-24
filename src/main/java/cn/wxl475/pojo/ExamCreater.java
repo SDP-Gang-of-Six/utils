37a3887155d1370aae835bef0ed72a2b3fe678a9
Cbgogogog <1293543528@qq.com>
2024-03-24 20:38:27 +0800

    feat: exam完善

--- src/main/java/cn/wxl475/pojo/ExamCreater.java
+++ src/main/java/cn/wxl475/pojo/ExamCreater.java
@@ -1,23 +1,14 @@
 package cn.wxl475.pojo;
 
-import com.baomidou.mybatisplus.annotation.FieldFill;
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableField;
-import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class ExamCreater {
     private Long examId;
-    private LocalDateTime submitTime;
-    private Long paperId;
-    private LocalDateTime startTime;
-    private Integer duration;
     private ArrayList<ExamDetailCreater> examDetailCreaters;
 }
