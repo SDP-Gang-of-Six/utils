bb2500c3e9d4d74ee7621092d3e1c732f688f73c
I578128 <xinglong.wang@sap.com>
2024-03-25 16:04:47 +0800

    增加video模块

---
+++ src/main/java/cn/wxl475/pojo/data/Video.java
@@ -0,0 +1,44 @@
+package cn.wxl475.pojo.data;
+
+import com.baomidou.mybatisplus.annotation.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@TableName(value = "videos",schema = "data")
+@Document(indexName = "videos")
+public class Video {
+    //对应数据库中的主键（UUID、自增id、雪花算法、redis、zookeeper）
+    @TableId(type = IdType.INPUT)
+    @Id
+    @Field(type = FieldType.Long, store = true,index = false)
+    private Long videoId;
+    @Field(type = FieldType.Long, store = true,index = false)
+    private Long userId;
+    @Field(type = FieldType.Text, store = true, analyzer = "ik_max_word",searchAnalyzer = "ik_smart")
+    private String videoUrl;
+    @Field(type = FieldType.Text, store = true, analyzer = "ik_max_word",searchAnalyzer = "ik_smart")
+    private String videoName;
+    @Field(type = FieldType.Text, store = true, analyzer = "ik_max_word",searchAnalyzer = "ik_smart")
+    private String videoType;
+    @Field(type = FieldType.Long, store = true,index = false)
+    private Long videoSize;
+    //注解填充字段 @TableField(.. fill = FieldFill.INSERT) 生成器策略部分也可以配置！
+    @TableField(fill = FieldFill.INSERT)
+    @Field(type = FieldType.Text, store = true, analyzer = "ik_max_word",searchAnalyzer = "ik_smart")
+    private String createTime;
+    //注解填充字段 @TableField(.. fill = FieldFill.INSERT) 生成器策略部分也可以配置！
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    @Field(type = FieldType.Text, store = true, analyzer = "ik_max_word",searchAnalyzer = "ik_smart")
+    private String updateTime;
+    @TableLogic
+    @Field(type = FieldType.Boolean, store = true,index = false)
+    private Boolean Deleted;
+}
