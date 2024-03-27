64aecce7dd10231d514db0777b965e8009b10935
wxl <1715142548@qq.com>
2024-03-27 15:26:00 +0800

    科室Department

---
+++ src/main/java/cn/wxl475/pojo/Department.java
@@ -0,0 +1,11 @@
+package cn.wxl475.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Department {
+}
