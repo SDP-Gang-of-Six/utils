8c4eee08f6d888b06958731963ac9b5bc5448ce6
wxl <1715142548@qq.com>
2024-03-27 17:05:58 +0800

    科室Department

---
+++ src/main/java/cn/wxl475/pojo/base/department/Department.java
@@ -0,0 +1,20 @@
+package cn.wxl475.pojo.base.department;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Department {
+    @TableId(type = IdType.ASSIGN_ID)
+    @Id
+    @Field(type = FieldType.Long, store = true,index = false)
+    private Long departmentId;
+}
