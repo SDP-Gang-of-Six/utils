f6b2466f1efe00285e21e671008fd58179d05cf0
wxl <1715142548@qq.com>
2024-03-31 14:28:43 +0800

    department建库建索引

--- src/main/java/cn/wxl475/pojo/base/Department.java
+++ src/main/java/cn/wxl475/pojo/base/Department.java
@@ -1,20 +1,35 @@
 package cn.wxl475.pojo.base;
 
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
 import org.springframework.data.elasticsearch.annotations.FieldType;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@TableName(value = "departments",schema = "base")
+@Document(indexName = "departments")
 public class Department {
     @TableId(type = IdType.ASSIGN_ID)
     @Id
     @Field(type = FieldType.Long, store = true,index = false)
     private Long departmentId;
+    @Field(type = FieldType.Text, store = true, analyzer = "ik_max_word",searchAnalyzer = "ik_smart")
+    private String departmentName;
+    //注解填充字段 @TableField(.. fill = FieldFill.INSERT) 生成器策略部分也可以配置！
+    @TableField(fill = FieldFill.INSERT)
+    @Field(type = FieldType.Text, store = true, analyzer = "ik_max_word",searchAnalyzer = "ik_smart")
+    private String createTime;
+    //注解填充字段 @TableField(.. fill = FieldFill.INSERT) 生成器策略部分也可以配置！
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    @Field(type = FieldType.Text, store = true, analyzer = "ik_max_word",searchAnalyzer = "ik_smart")
+    private String updateTime;
+    @TableLogic
+    @Field(type = FieldType.Boolean, store = true,index = false)
+    private Boolean Deleted;
 }
