b558dc90b8ab007946e8c3fc7421abb9e2d92d6e
Cbgogogog <1293543528@qq.com>
2024-04-02 02:59:26 +0800

    feat: 住院疫苗

---
+++ src/main/java/cn/wxl475/pojo/base/Vaccine.java
@@ -0,0 +1,39 @@
+package cn.wxl475.pojo.base;
+
+import com.baomidou.mybatisplus.annotation.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@TableName(value = "vaccine",schema = "base")
+@Document(indexName = "vaccine")
+public class Vaccine {
+    @TableId(type = IdType.ASSIGN_ID)
+    @Id
+    @Field(type = FieldType.Long, store = true,index = false)
+    private Long vaccineId;
+    @Field(type = FieldType.Text, store = true, analyzer = "ik_max_word",searchAnalyzer = "ik_smart")
+    private String vaccineName;
+    @Field(type = FieldType.Keyword, store = true,index = false)
+    private String vaccineUsage;
+    @Field(type = FieldType.Text, store = true, analyzer = "ik_max_word",searchAnalyzer = "ik_smart")
+    private String vaccineDetail;
+    //注解填充字段 @TableField(.. fill = FieldFill.INSERT) 生成器策略部分也可以配置！
+    @TableField(fill = FieldFill.INSERT)
+    @Field(type = FieldType.Keyword, store = true,index = false)
+    private String createTime;
+    //注解填充字段 @TableField(.. fill = FieldFill.INSERT) 生成器策略部分也可以配置！
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    @Field(type = FieldType.Keyword, store = true,index = false)
+    private String updateTime;
+    @TableLogic
+    @Field(type = FieldType.Boolean, store = true,index = false)
+    private Boolean Deleted;
+}
