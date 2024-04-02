9ae1e4df7a6a42bcd8e2bcb9dfe7d4a09e58dc77
wxl <1715142548@qq.com>
2024-04-02 10:32:27 +0800

    data依赖注入改为构造器注入

--- src/main/java/cn/wxl475/pojo/base/Hospitalization.java
+++ src/main/java/cn/wxl475/pojo/base/Hospitalization.java
@@ -1,14 +1,8 @@
 package cn.wxl475.pojo.base;
 
 import com.baomidou.mybatisplus.annotation.TableName;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.springframework.data.elasticsearch.annotations.Document;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 @TableName(value = "hospitalization",schema = "base")
 @Document(indexName = "hospitalization")
 public class Hospitalization {
