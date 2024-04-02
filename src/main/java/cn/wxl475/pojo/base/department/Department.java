fdd5f445833eb5a35c1f5caf5397e16f8abf700a
wxl <1715142548@qq.com>
2024-04-02 15:16:50 +0800

    科室子类

--- src/main/java/cn/wxl475/pojo/base/department/Department.java
+++ src/main/java/cn/wxl475/pojo/base/department/Department.java
@@ -27,6 +27,8 @@ public class Department {
     private String departmentPrincipal;
     @Field(type = FieldType.Text, store = true, analyzer = "ik_max_word",searchAnalyzer = "ik_smart")
     private String departmentFunction;
+    @Field(type = FieldType.Integer, store = true,index = false)
+    private Integer departmentRoomNumber;
     //注解填充字段 @TableField(.. fill = FieldFill.INSERT) 生成器策略部分也可以配置！
     @TableField(fill = FieldFill.INSERT)
     @Field(type = FieldType.Keyword, store = true,index = false)
