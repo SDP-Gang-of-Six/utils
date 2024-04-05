db26b607c016790ac1cebc4167286336a358118c
wxl <1715142548@qq.com>
2024-04-05 19:18:24 +0800

    科室人员取消软删除

--- src/main/java/cn/wxl475/pojo/base/Staff.java
+++ src/main/java/cn/wxl475/pojo/base/Staff.java
@@ -37,7 +37,4 @@ public class Staff {
     @TableField(fill = FieldFill.INSERT_UPDATE)
     @Field(type = FieldType.Keyword, store = true,index = false)
     private String updateTime;
-    @TableLogic
-    @Field(type = FieldType.Boolean, store = true,index = false)
-    private Boolean Deleted;
 }
