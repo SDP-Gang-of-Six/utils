158ad7cff971fa9d258b1de612a487ffe203ae1b
wxl <1715142548@qq.com>
2024-04-05 19:19:34 +0800

    科室人员取消软删除

--- src/main/java/cn/wxl475/pojo/base/department/SubDepartment.java
+++ src/main/java/cn/wxl475/pojo/base/department/SubDepartment.java
@@ -6,7 +6,7 @@ public class SubDepartment <DATA> extends Department{
     List<DATA> data;
 
     public SubDepartment(Department department, List<DATA> data) {
-        super(department.getDepartmentId(), department.getDepartmentName(), department.getDepartmentType(), department.getDepartmentPrincipal(), department.getDepartmentFunction(), department.getDepartmentRoomNumber(), department.getCreateTime(), department.getUpdateTime(), department.getDeleted());
+        super(department.getDepartmentId(), department.getDepartmentName(), department.getDepartmentType(), department.getDepartmentPrincipal(), department.getDepartmentFunction(), department.getDepartmentRoomNumber(), department.getCreateTime(), department.getUpdateTime());
         this.data = data;
     }
 }
